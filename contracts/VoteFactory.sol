dd09341ed0410a7ea8e4bac77b2a48aa5ce282b8
Lucas Grasso <lucasgrassoramos@gmail.com>
2022-06-17 12:30:11 -0300

    time logic, initialization

--- contracts/VoteFactory.sol
+++ contracts/VoteFactory.sol
@@ -4,14 +4,23 @@ pragma solidity ^0.8.0;
 import "./EIP1167.sol";
 import "./Vote.sol";
 
+/**
+ * @dev interface to interact with cloned Vote contracts. Used to call the initialize() function
+ *      given that cloned contracts cant have constructors.
+ *
+ */
 interface VoteInit {
-    function initialize(address _sender) external;
+    function initialize( 
+        uint256 _votingCost,
+        uint256 _minVotes,
+        uint256 _timeToVote,
+        address _sender)  external;
 }
 
 contract VoteFactory is MinimalProxy {
 
   address public owner;
-  address public impl;
+  address public impl; //Adress of the vote contract to be cloned
 
   constructor (address _impl) {
     owner = msg.sender;
@@ -19,7 +28,7 @@ contract VoteFactory is MinimalProxy {
   }
 
   modifier onlyOwner() {
-    require(msg.sender == owner,"Access denied");
+    require(msg.sender == owner, "Access denied");
     _;
   }
 
@@ -30,16 +39,15 @@ contract VoteFactory is MinimalProxy {
    */
   function changeImpl(address _newImpl) public onlyOwner {
     impl = _newImpl;
-    return;
   }
 
   /**
    * @dev clone and init Vote function
+   * 
    */
-  function createVote() public payable {
+  function createVote(uint256 _votingCost, uint256 _minVotes, uint256 _timeToVote) public payable {
+    require(_votingCost > 0, "Voting cost can't be 0");
     address voteproxy = this.deployMinimal(impl);
-    VoteInit(voteproxy).initialize(msg.sender);
-    return;
+    VoteInit(voteproxy).initialize(_votingCost, _minVotes, _timeToVote, msg.sender);
   }
-  
 }
