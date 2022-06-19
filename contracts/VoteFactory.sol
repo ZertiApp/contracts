8d1780535510e6e3d703062692774265552c71ab
Lucas Grasso <lucasgrassoramos@gmail.com>
2022-06-19 20:57:17 -0300

    revert changes

--- contracts/VoteFactory.sol
+++ contracts/VoteFactory.sol
@@ -62,21 +62,13 @@ contract VoteFactory is MinimalProxy {
     ) public payable {
         if (_votingCost > 0 || _minVotes > 50 || _timeToVote > 5)
             revert InvalidVote();
-        _createVote (_votingCost, _minVotes, _timeToVote, msg.sender);
-    }
 
-    function _createVote (
-        uint256 _votingCost,
-        uint256 _minVotes,
-        uint256 _timeToVote,
-        address _sender
-    ) internal {
         address voteproxy = this.deployMinimal(voteImpl);
         VoteInit(voteproxy).initialize(
             _votingCost,
             _minVotes,
             _timeToVote,
-            _sender
+            msg.sender
         );
     }
 
