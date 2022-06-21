9d53b6418ef38af44329b78041c594b83df5dc24
MatiArazi <71347020+MatiArazi@users.noreply.github.com>
2022-06-21 09:24:01 -0300

    Update Badge.sol

---
+++ contracts/Badge.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+
+/**
+ * @title Badge contract
+ * @author Zerti Team - Matias Arazi
+ * @notice Badge contract use for storing your badges as SBTs
+ */
+pragma solidity ^0.8.4;
+
+contract Badges {
+    uint256 idCount = 0;
+    struct Badge{
+        uint256 id;
+        string name;
+        string description;
+        string image;
+    }
+
+    mapping(uint256 => Badge) badges; //id to badge
+
+    function mint(string memory _name, string _description, string _image) {
+        idCount++;
+        badges[idCount] = Badge(idCount, _name, _description, _image);
+    }
+
+    function receive() {
+
+    }
+
+    function burn(uint256 memory _id){
+        badges[_id].
+    }
+
+}
