aa933e7c6c1ca087db9fcf7bc89fb889801f4807
MatiArazi <71347020+MatiArazi@users.noreply.github.com>
2022-06-27 08:35:25 -0300

    Zerti contract

--- contracts/Badge.sol
+++ contracts/Badge.sol
@@ -9,7 +9,45 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract Badge is ERC1155 {
+contract Zerti {
+    uint256 private nonce;
+    struct Zerti {
+        address owner;
+        string data;
+    }
+    mapping(uint256 => Zerti) zerties;
+    mapping(uint256 => uint256) amount;
+    mapping(address => mapping(uint256 => bool)) owners;
+    mapping(address => mapping(uint256 => bool)) pending;
+
+    function mint(string data) public {
+        zerties[++nonce] = Zerti(msg.sender, data);
+        amount[nonce] = 0;
+    }
+
+    function transfer(uint256 memory id, address to[]) public {
+        require(zerties[id].owner == msg.sender);
+        for(int dest = 0; dest<to.size(); dest++){
+            require(owners[id][dest] == false);
+            require(pending[id][dest] == false);
+            pending[dest] = id;
+        }
+    }
+
+    function claim(uint memory id){
+        require(owners[id][dest] == false);
+        require(pending[id][dest] == true);
+        amount[id]++;
+        owners[msg.sender][id] = true;
+        pending[msg.sender][id] = false;
+    }
+
+    function burn(uint256 memory id){
+        require(owners[id][dest] == true);
+        amount[id]--;
+        owners[msg.sender][id] = false;
+    }
+
     uint256 public constant ort = 1;
     uint256 public constant oma = 2;
     uint256 public constant launchpad = 3;
