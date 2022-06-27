a08de6af1481ea366b7d5483095aa23c5659d94b
MatiArazi <71347020+MatiArazi@users.noreply.github.com>
2022-06-27 13:50:50 -0300

    zerti

--- contracts/Badge.sol
+++ contracts/Badge.sol
@@ -7,56 +7,73 @@
  */
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract Zerti {
     uint256 private nonce;
-    struct Zerti {
+
+    struct zerti {
         address owner;
         string data;
     }
-    mapping(uint256 => Zerti) zerties; // id to Zerti
+
+    error Unauthorized(address _sender);
+    error NotOwned();
+
+
+    mapping(uint256 => zerti) zerties; // id to Zerti
     mapping(uint256 => uint256) amount;// the amounts of tokens for each Zerti
     mapping(address => mapping(uint256 => bool)) owners;// if owner has a specific Zerti
     mapping(address => mapping(uint256 => bool)) pending;// if owner has pending a specific Zerti
 
-    function mint(string data) public {
-        zerties[++nonce] = Zerti(msg.sender, data);
+    
+    function idInfo(uint id) external view returns(address, string memory){
+        return (zerties[id].owner, zerties[id].data);
+    }
+
+    /*
+    uint[] myzerti;
+    function zertiesFrom(address from) external view returns(uint[] memory){
+        myzerti = [];
+        for(uint i = 1; i<=nonce; i++){
+            if(owners[from][i]){
+                myzerti.push(i);
+            }
+        }
+        return myzerti;
+    }
+    */
+    function mint(string calldata data) public {
+        //requiere be entity
+        zerties[++nonce] = zerti(msg.sender, data);
         amount[nonce] = 0;
     }
 
-    function transfer(uint256 memory id, address to[]) public {
-        require(zerties[id].owner == msg.sender);
-        for(int dest = 0; dest<to.size(); dest++){
-            require(owners[id][dest] == false);
-            require(pending[id][dest] == false);
-            pending[dest] = id;
+    function transfer(uint256 id, address[] memory to) public {
+        if(zerties[id].owner != msg.sender) revert Unauthorized(msg.sender);
+        for(uint i = 0; i<to.length;){
+            address dest = to[i];
+            if(owners[dest][id] != false || pending[dest][id] )
+            require(owners[dest][id] == false);
+            require(pending[dest][id] == false);
+            pending[dest][id] = true;
+            unchecked{
+                ++i;
+            }
         }
     }
 
-    function claim(uint memory id){
-        require(owners[id][dest] == false);
-        require(pending[id][dest] == true);
-        amount[id]++;
+    function claim(uint id) public{
+        require(owners[msg.sender][id] == false);
+        require(pending[msg.sender][id] == true);
         owners[msg.sender][id] = true;
         pending[msg.sender][id] = false;
+        amount[id]++;
     }
 
-    function burn(uint256 memory id){
-        require(owners[id][dest] == true);
-        amount[id]--;
+    function burn(uint256 id) public{
+        require(owners[msg.sender][id] == true);
         owners[msg.sender][id] = false;
-    }
-    /*
-    uint256 public constant ort = 1;
-    uint256 public constant oma = 2;
-    uint256 public constant launchpad = 3;
-    uint256 public constant coderhouse = 4;
+        amount[id]--;
 
-    constructor() ERC1155("test/BadgesJSON/{id}.json") {
-        _mint(msg.sender, ort, 10**27, "");
-        _mint(msg.sender, oma, 1, "");
-        _mint(msg.sender, launchpad, 10**9, "");
-        _mint(msg.sender, coderhouse, 10**9, "");
-    }*/
+    }
 }
