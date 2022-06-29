f3f077b1e2ee3ff432d8aff8abc27dee587429df
MatiArazi <71347020+MatiArazi@users.noreply.github.com>
2022-06-29 15:41:56 -0300

    Update 3_BadgeTest.js

--- test/3_BadgeTest.js
+++ test/3_BadgeTest.js
@@ -2,50 +2,54 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-describe("Badge.sol", function () { 
-    let deployer, addr1, addr2, badge, addrs
+describe("Badge.sol", function () {
+    let owner, addr1, addr2, badge, addrs
     beforeEach(async function () {
         //Get contract factories
         const Badge = await ethers.getContractFactory("EIPARAZI");
 
         //Get signers
-        [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
         //Deploy contrats
         badge = await Badge.deploy();
-    })
-    describe("Mint zerties", async function () {
-        it("Should mint tokens", async function (){
+    });
+    describe("Minting", async function () {
+        it("Should mint tokens", async function () {
             await expect(badge.mint('ILUKIW'))
-        .to.emit(badge, "ZertiMinted")
-        .withArgs(
-            owner.address,
-            1,
-        )
-        await expect(badge.mint("watafak"))
-        .to.emit(badge, "ZertiMinted")
-        .withArgs(
-            owner.address,
-            2,
-        )
-        await expect(badge.mint("grays anatomy"))
-        .to.emit(badge, "ZertiMinted")
-        .withArgs(
-            owner.address,
-            3,
-        )
-        })
-        
-        it("Info of zerties ", async function(){
-            expect( await badge.uriOf(1)).to.equal('ILUKIW')
-            expect( await badge.uriOf(2)).to.equal("watafak")
-            expect( await badge.uriOf(3)).to.equal("grays anatomy")
+                .to.emit(badge, "ZertiMinted")
+                .withArgs(
+                    owner.address,
+                    1,
+                );
+            await expect(badge.mint("watafak"))
+                .to.emit(badge, "ZertiMinted")
+                .withArgs(
+                    owner.address,
+                    2,
+                );
+            await expect(badge.mint("grays-anatomy"))
+                .to.emit(badge, "ZertiMinted")
+                .withArgs(
+                    owner.address,
+                    3,
+                )
+        });
 
-            expect(await badge.amountOf(1)).to.equal(0)
-            expect(await badge.amountOf(2)).to.equal(0)
-            expect(await badge.amountOf(3)).to.equal(0)
+        it("Should init info correctly ", async function () {
+            const uris = ['ILUKIW', 'watafak', "grays-anatomy"];
+            for(let i = 0; i<uris.length; i++){
+                let mintTx = await badge.mint(uris[i]);
+                await mintTx.wait();
+            }
+
+            for(let i = 0; i<uris.length; i++){
+                expect(await badge.uriOf(i+1)).to.equal(uris[i]);
+                expect(await badge.amountOf(i+1)).to.equal(0);
+                expect(await badge.ownerOf(i+1)).to.equal(owner);
+            }
         })
     })
 })
