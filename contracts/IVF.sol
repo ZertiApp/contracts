23712a808ad1e7f1115bcc4574f13333450ac705
Lucas Grasso <lucasgrassoramos@gmail.com>
2022-06-30 11:22:29 -0300

    Update IVF.sol

--- contracts/IVF.sol
+++ contracts/IVF.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.4;
 
 /**
- * @dev Interface of the VoteFactory Conta
+ * @dev Interface of the VoteFactory Contract
  */
 interface IVF {
     function isEntity(address _addr) external view returns (bool);
