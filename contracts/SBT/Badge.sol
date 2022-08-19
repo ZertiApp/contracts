ef399cff1be15a89233dbc6a9a8718155004be20
"Lucas Grasso" <"lucasgrassoramos@gmail.com">
2022-08-19 19:34:08 -0300

    safeMultiTransfer -> multiTransfer

--- contracts/SBT/Badge.sol
+++ contracts/SBT/Badge.sol
@@ -21,9 +21,6 @@ contract Badge is SBTERC1155 {
     // Used as Contract Metadata URI
     string public _contractUri;
 
-    // Used to store contracts created by each address
-    mapping(address => uint256[]) internal _minterBalances;
-
     constructor(string memory _uri, string memory _name, string memory _symbol, string memory contractUri_) SBTERC1155(_uri) {
         name = _name;
         symbol = _symbol;
@@ -33,9 +30,7 @@ contract Badge is SBTERC1155 {
     // Mints (creates a token)
     function mint(string memory data) external {
         if(keccak256(abi.encodePacked(data)) == keccak256("")) revert("Data is empty");
-        address operator = _msgSender();
-        _mint(operator, data);
-        _minterBalances[operator].push(nonce);
+        _mint(msg.sender, data);
     }
 
     // Burns (deletes a token)
@@ -50,45 +45,6 @@ contract Badge is SBTERC1155 {
         return string(
                 abi.encodePacked("https://ipfs.io/ipfs/", _contractUri)
             );
-    }
-
-    /**
-     * @dev Retrieve ids of tokens minted by `account`
-
-     * Requirements:
-     *
-     * - `account must be non-zero`
-     *
-     */
-    function mintedBy(address account) external view returns(uint256[] memory) {
-        if(account == address(0)) revert AddressZero();
-        return _minterBalances[account];
-    }
-
-    /**
-     * @dev Retrieve URIs of tokens minted by `account`
-
-     * Requirements:
-     *
-     * - `account must be non-zero`
-     *
-     */
-    function mintedByURIS(address account) external view returns(string[] memory) {
-        if(account == address(0)) revert AddressZero();
-        uint256[] memory _mintedTokens = _minterBalances[account];
-        uint256 _nMintedTokens = _mintedTokens.length;
-        string[] memory _mintedTokensURI = new string[] (_nMintedTokens);
-
-        for(uint256 i = 0; i < _nMintedTokens;){
-            _mintedTokensURI[i] = string(
-                abi.encodePacked(_uri, tokens[_mintedTokens[i]].data)
-            );
-            unchecked {
-                ++i;
-            }
-        }
-
-        return _mintedTokensURI;
     }
 
 }
